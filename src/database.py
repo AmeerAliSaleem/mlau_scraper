52311e14cf58dc39c1f42709ece78336759ed518
Ameer <Ameer.Saleem@warwick.ac.uk>
2026-06-06 20:03:20 +0100

    Implemented functions to access, remove and upsert Supabase records.

--- src/database.py
+++ src/database.py
@@ -1,9 +1,12 @@
+"""Functions to handle interactions with Supabase"""
+
 import os
 import logging
-import pandas as pd
+from dotenv import load_dotenv
 from supabase import create_client, Client
 
 logger = logging.getLogger(__name__)
+load_dotenv()
 
 def get_supabase_client() -> None | Client:
     """
@@ -12,8 +15,8 @@ def get_supabase_client() -> None | Client:
     Returns:
         Supabase client if credentials are available, None otherwise.
     """
-    url = os.environ.get('SUPABASE_URL')
-    key = os.environ.get('SUPABASE_KEY')
+    url = os.getenv('SUPABASE_URL')
+    key = os.getenv('SUPABASE_KEY')
 
     if not url or not key:
         logger.error('Supabase credentials not available')
@@ -21,25 +24,60 @@ def get_supabase_client() -> None | Client:
 
     return create_client(url, key)
 
-def access_supabase_data():
-    pass
-
-def save_to_supabase(
-        df: pd.DataFrame,
-        table_name: str
-) -> None:
+def access_supabase_data(table_name: str):
+    """Retrieve data from the given table."""
     supabase: Client = get_supabase_client()
-    if supabase is None:
-        raise ValueError("Supabase client not available. Missing environmental variables SUPABASE_URL and/or SUPABASE_KEY")
 
-    logger.info('Saving predictions to Supabase...')
-    response = (
+    logger.info(f'Reading data from {table_name} table...')
+    df = (
         supabase.table(table_name)
-        .insert(df)
+        .select('*')
         .execute()
     )
 
-    logger.info(f'Successfully saved prediction to Supabase')
+    return df
+
+def clear_supabase(table_name: str) -> None:
+    """Refresh the current state of the 'Top posts' table."""
+    supabase: Client = get_supabase_client()
+
+    logger.info(f'Clearing {table_name} table...')
+    _ = (
+        supabase.table(table_name)
+        .delete()
+        .neq('id', 0)
+        .execute()
+    )
+
+def save_to_supabase(
+        df: list[dict[str, any]],
+        table_name: str,
+        upsert: bool = False,
+) -> None:
+    """
+    Save DataFrame (in json format) to Supabase table.
+    """
+    supabase: Client = get_supabase_client()
+
+    logger.info(f'Saving records to {table_name} table...')
+
+    if upsert:
+        _ = (
+            supabase.table(table_name)
+            .upsert(
+                df,
+                on_conflict='id'
+            )
+            .execute()
+        )
+    else:
+        _ = (
+            supabase.table(table_name)
+            .insert(df)
+            .execute()
+        )
+
+    logger.info(f'Successfully saved records to Supabase')
 
 if __name__ == '__main__':
     get_supabase_client()
